c82b6205bd738ddc1f988f5242d4966b10954843
Yaroslav <yaroslav.kerez@gmail.com>
2024-08-22 13:42:16 +0200

    Add project files.

---
+++ Market/Customer/Customer.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Market.Customer
+{
+    public partial class Customer : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Url.AbsoluteUri.ToString().Contains("DefaultCustomer.aspx"))
+            {
+                //load the usercontrol
+                Control sliderUserControl = (Control)Page.LoadControl("BannerUserControl.ascx");
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
---
+++ Market/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Market.Global" Language="C#" %>
